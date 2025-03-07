683fd890b70c8d628b3a74730efb4389be570dcd
Iamlucyferr <lucyferkh@gmail.com>
2025-03-07 14:45:41 +0700

    Add files via upload

---
+++ clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('play.dobii.club')
+    swal({
+    title: 'IP COPIATO',
+    text: 'Copying was successful.',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
